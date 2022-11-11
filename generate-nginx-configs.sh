da3448a4bbd10b651d5096b9d706182e7e3b56c2
andrei petrov <ndt.080@yandex.by>
2022-11-11 14:44:48 +0000

    Init

---
+++ generate-nginx-configs.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# VARIABLES
+
+#=======================================================================================
+# ATTENTION!
+# The order of variable domains in the array is important!
+# RELEASE_DOMAIN, STAGING_DOMAIN, DEV_DOMAIN
+#=======================================================================================
+
+PROJECT_DOMAINS=(
+    "acvod.alignedcode.com" 
+    "acvod-staging.alignedcode.com" 
+    "acvod-dev.alignedcode.com"
+)
+
+#=======================================================================================
+# ATTENTION!
+# SERVER PROJECT PORTS 800*
+# CLIENT PROJECT PORTS 808*
+
+# The order of variable ports in the array is important!
+# RELEASE_DOMAIN, STAGING_DOMAIN, DEV_DOMAIN
+#=======================================================================================
+
+PROJECT_PORTS=(
+    "8082" 
+    "8081" 
+    "8080" 
+)
+
+
+#=======================================================================================
+#
+#
+# ATTENTION! WE DO NOT CHANGE THE SCRIPT FURTHER
+#
+#
+#=======================================================================================
+
+__nginx="
+user root;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format main '\$remote_addr - \$remote_user [\$time_local] \"\$request\" '
+                    '\$status \$body_bytes_sent \"\$http_referer\" '
+                    '\"\$http_user_agent\" \"\$http_x_forwarded_for\"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 4096;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    include /etc/nginx/conf.d/*.conf;   
+
+    server {
+        listen       80;
+        server_name  '';
+        gzip_static on;
+
+        location / {
+            return 301 https://\$server_name\$request_uri;
+        }
+    }
+"
+
+__nginx_close="
+}
+"
+
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e "\033[32m[                     NGINX CONFIG GENERATOR                    ]\033[m"
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e " Starting the generations..." && echo
+
+# Delete nginx folder, if there is one
+rm -r nginx
+# Create ndinx folder
+mkdir nginx
+
+__nginx_ssl="$__nginx"
+__nginx_no_ssl="$__nginx"
+
+for i in ${!PROJECT_DOMAINS[@]}; do
+
+server_record="
+    server {
+        listen       80;
+        server_name  ${PROJECT_DOMAINS[$i]};
+        gzip_static on;
+
+        location / {
+            proxy_pass http://127.0.0.1:${PROJECT_PORTS[$i]}\$request_uri;
+        }
+    }
+"
+
+server_record_ssl="
+    server {
+        listen       443 ssl;
+        server_name  ${PROJECT_DOMAINS[$i]};
+        ssl_certificate /etc/letsencrypt/live/${PROJECT_DOMAINS[$i]}/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/${PROJECT_DOMAINS[$i]}/privkey.pem;
+        
+        add_header Strict-Transport-Security \"max-age=31536000\";
+        gzip_static on;
+
+        location / {
+            proxy_pass http://127.0.0.1:${PROJECT_PORTS[$i]}\$request_uri;
+        }
+    }
+"
+
+__nginx_no_ssl="${__nginx_no_ssl}${server_record}"
+__nginx_ssl="${__nginx_ssl}${server_record}${server_record_ssl}"
+done
+
+echo "$__nginx_no_ssl$__nginx_close" >> nginx/nginx.conf
+echo "$__nginx_ssl$__nginx_close" >> nginx/ssl.nginx.conf
+
+echo -e "\033[32mNginx config files generation completed successfully!\033[m" && echo
---
+++ init-system.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# VARIABLES
+PROJECT_DOMAIN_RELEASE="acvod.alignedcode.com"
+PROJECT_DOMAIN_STAGING="acvod-staging.alignedcode.com"
+PROJECT_DOMAIN_DEV="acvod-dev.alignedcode.com"
+
+
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e "\033[32m[STEP 0. UPDATE AND INIT SYSTEM]\033[m"
+echo -e "\033[32m[===============================================================]\033[m"
+
+# Update system
+sudo apt-get update                 # check updates
+sudo apt-get upgrade                # install updates
+echo -e "\033[32mThe system has been updated!\033[m" && echo
+
+# Create swap file
+free -h
+sudo fallocate -l 5G /swapfile      # create swapfile
+sudo chmod 600 /swapfile            # leave access only to the superuser
+sudo mkswap /swapfile               # create swap filesystem
+sudo swapon /swapfile               # enable swapfile
+free -h
+echo -e "\033[32mThe swap section has been created!\033[m" && echo
+
+# make the swap file work after the system reboot
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab 
+
+# Grant access to the firewall for ssh connection
+sudo ufw allow 'OpenSSH'
+
+# Enable firewall
+sudo ufw allow 5432/tcp
+sudo ufw enable
+echo -e "\033[32mThe firewall has been enabled!\033[m" && echo
+
+
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e "\033[32m[STEP 1. INSTALL NGINX]\033[m"
+echo -e "\033[32m[===============================================================]\033[m"
+
+# Install Nginx
+sudo apt install nginx
+
+# Grant access to the firewall for Nginx
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTPS'
+
+# Check firewall status
+sudo ufw status
+
+# Check nginx status
+systemctl status nginx | cat
+echo -e "\033[32mThe Nginx has been installed!\033[m" && echo
+
+
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e "\033[32m[STEP 2. CONFIGURATING NGINX AND SSL]\033[m"
+echo -e "\033[32m[===============================================================]\033[m"
+
+# Install an ssl certificate provider 
+sudo apt-get install certbot
+sudo apt-get install python3-certbot-nginx
+
+# Update nginx config
+cp nginx/nginx.conf /etc/nginx/nginx.conf
+
+# Checking the validity of nginx.conf and restart nginx
+nginx -t && nginx -s reload
+
+sudo certbot --nginx -d $PROJECT_DOMAIN_DEV     --register-unsafely-without-email
+sudo certbot --nginx -d $PROJECT_DOMAIN_STAGING --register-unsafely-without-email
+sudo certbot --nginx -d $PROJECT_DOMAIN_RELEASE --register-unsafely-without-email
+
+# Update nginx config
+cp nginx/ssl.nginx.conf /etc/nginx/nginx.conf
+
+# Checking the validity of nginx.conf and restart nginx
+nginx -t && nginx -s reload
+
+
+echo -e "\033[32m[===============================================================]\033[m"
+echo -e "\033[32m[STEP 3. INSTALL DOCKER]\033[m"
+echo -e "\033[32m[===============================================================]\033[m"
+
+# Set up the docker repository
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo -e "\033[32mThe Docker has been installed!\033[m" && echo
